4019cba36130ebba9d0fcc67936bc44d6ae2b783
Shadi Shwiyat <118314635+Shadi-Shwiyat@users.noreply.github.com>
2023-07-05 15:38:14 -0500

    Revert "updated init file"
    This reverts commit d4f2f9863173910082716a8829ce72877d6911ac.

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""Initializes models module"""
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
-if os.getenv('HBNB_TYPE_STORAGE') == "db":
+
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
