b7275c6345fb62253a896e85b856b87dbaa65eaa
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-05 12:17:11 -0700

    fixing console error

--- console.py
+++ console.py
@@ -3,7 +3,6 @@
 import cmd
 import sys
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -33,10 +32,11 @@ class HBNBCommand(cmd.Cmd):
     def preloop(self):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb)')
+            print('(hbnb) ', end="")
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
+
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
@@ -72,7 +72,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] == '}'\
+                    # added space around second curly brace in line below
+                    if pline[0] == '{' and pline[-1] == '}' \
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,46 +115,32 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        pline = args.split()
-        _cls = pline[0]
-        values = []
-        names = []
-        if not _cls:
+        from models import storage
+        list_args = args.split()
+        if not args:
             print("** class name missing **")
             return
-        elif _cls not in HBNBCommand.classes:
+        elif list_args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        for i in range(1, len(pline)):
-            tupl = pline[i].partition('=')
-            names.append(tupl[0])
-            try:
-                if tupl[2][0] == '\"' and tupl[2][-1] == '\"':
-                    value = tupl[2].replace('\"', '')
-                    value = value.replace('_', ' ')
-                    values.append(value)
-                else:
-                    value = tupl[2]
-                    if '.' in value or type(value) is float:
-                        try:
-                            value = float(value)
-                            values.append(value)
-                        except Exception:
-                            pass
-                    else:
-                        try:
-                            value = int(value)
-                            values.append(value)
-                        except Exception:
-                            pass
-            except IndexError:
-                continue
+        new_instance = HBNBCommand.classes[list_args[0]]()
+        for arg in range(1, len(list_args)):
+            key_value_pair = list_args[arg].partition("=")
+            key_name = key_value_pair[0]
+            key_value = key_value_pair[2]
+            if '\"' in key_value:
+                key_value = key_value[1:-1]
+                key_value = key_value.replace("_", " ")
+            elif "." in key_value:
+                key_value = float(key_value)
+            else:
+                key_value = int(key_value)
 
-        dictionary = dict(zip(names, values))
+            if hasattr(new_instance, key_name):
+                setattr(new_instance, key_name, key_value)
 
-        new_instance = HBNBCommand.classes[_cls]()
-        new_instance.__dict__.update(dictionary)
-        new_instance.save()
+        storage.new(new_instance)
+        storage.save()
         print(new_instance.id)
 
     def help_create(self):
@@ -163,6 +150,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """ Method to show an individual object """
+        from models import storage
         new = args.partition(" ")
         c_name = new[0]
         c_id = new[2]
@@ -196,6 +184,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, args):
         """ Destroys a specified object """
+        from models import storage
         new = args.partition(" ")
         c_name = new[0]
         c_id = new[2]
@@ -229,18 +218,20 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
+        from models import storage
         print_list = []
+        objects = storage.all()
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all(HBNBCommand.classes[args]).items():
+            for k, v in objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage.all().items():
+            for k, v in objects.items():
                 print_list.append(str(v))
 
         print(print_list)
@@ -252,8 +243,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_count(self, args):
         """Count current number of class instances"""
+        from models import storage
         count = 0
-        for k, v in storage._FileStorage__objects.items():
+        for k, v in storage.all().items():
             if args == k.split('.')[0]:
                 count += 1
         print(count)
@@ -264,6 +256,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, args):
         """ Updates a certain object with new info """
+        from models import storage
         c_name = c_id = att_name = att_val = kwargs = ''
 
         # isolate cls from id/args, ex: (<cls>, delim, <id/args>)
@@ -349,6 +342,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+# added second blank line here to satisfy pycodestyle
 
 
 if __name__ == "__main__":
