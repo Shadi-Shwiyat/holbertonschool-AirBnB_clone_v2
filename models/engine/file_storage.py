724f8887d3f64990a5cebea9b441648d7ccb3307
Shadi Shwiyat <118314635+Shadi-Shwiyat@users.noreply.github.com>
2023-07-05 15:38:27 -0500

    Revert "Fixing file storage errors"
    This reverts commit 37e7819441b28f56c3d6de8dc40601a938d20b73.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,21 +9,25 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """
-        If cls=None, returns a dictionary of models
-        currently in storage, else returns a dictionary
-        of models with class=cls
-        """
-        # print(FileStorage.__objects)
-        if cls is None:
-            return FileStorage.__objects
+        """Returns a dictionary of models currently in storage"""
+        dictionary = {}
+        if cls:
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    dictionary[key] = value
+            return dictionary
+        return FileStorage.__objects
 
-        cls_objects = {}
-        for value in FileStorage.__objects.values():
-            if type(value) == cls:
-                cls_objects.update({value.to_dict()['__class__'] +
-                                    '.' + value.id: value})
-        return cls_objects
+    def delete(self, obj=None):
+        """Deletes an object from the dictionary"""
+        if obj:
+            for key, value in FileStorage.__objects.items():
+                if value == obj:
+                    del FileStorage.__objects[key]
+                    self.save()
+                    return
+        else:
+            pass
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -39,7 +43,11 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """deserializes the JSON file to __objects
+        - only if the JSON file (__file_path) exists
+        - otherwise, do nothing.
+        - If the file doesn’t exist, no exception should be raised
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -48,31 +56,18 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        classes = {"BaseModel": BaseModel, "User": User, "State": State,
+                   "Amenity": Amenity, "Place": Place, "City": City,
+                   "Review": Review}
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding='UTF-8') as file:
+                js = json.load(file)
+            for key, value in js.items():
+                reloadobj = classes[js[key]["__class__"]](**js[key])
+                self.__objects[key] = reloadobj
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """deletes an object from __objects"""
-        if obj is None:
-            return
-        for key, value in dict(FileStorage.__objects).items():
-            if value == obj:
-                del FileStorage.__objects[key]
-
-    def close(self):  # p1170t7
-        """
-        Calls the reload() method for de-
-        serializing the JSON file to objects.
-        """
+    def close(self):
+        """method to reload"""
         self.reload()
