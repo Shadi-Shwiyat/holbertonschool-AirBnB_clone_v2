895b60dff923727b9e7bc12ae3089f75edc71b6b
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-05 12:57:18 -0700

    updated place and amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class, contains amenity name"""
-    from models.place import place_amenity
+    """Contains amenities class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity)
+
+    # if getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     place_amenities = relationship("Place", secondary='place_amenity',
+    #                                    back_populates="amenities")
