1e1cb0a0f35a81f8cecd97e14c36a3df09adcc2a
Shadi Shwiyat <118314635+Shadi-Shwiyat@users.noreply.github.com>
2023-07-05 15:38:12 -0500

    Revert "updated place and amenity"
    This reverts commit 895b60dff923727b9e7bc12ae3089f75edc71b6b.

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """Contains amenities class"""
+    """Amenity class, contains amenity name"""
+    from models.place import place_amenity
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    # if getenv('HBNB_TYPE_STORAGE') == 'db':
-    #     place_amenities = relationship("Place", secondary='place_amenity',
-    #                                    back_populates="amenities")
+    place_amenities = relationship('Place', secondary=place_amenity)
