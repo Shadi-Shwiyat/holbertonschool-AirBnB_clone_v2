75bec8efab9a0e56ac73b82d48c93a1d726e0872
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-16 17:21:30 -0700

    Added python route with default text "is cool"

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""  Script that starts a Flask web application """
+
+from flask import Flask
+
+
+def start_flask():
+    """ Start a Flask web application """
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def hello():
+        """ Display 'Hello HBNB!' """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """ Display "HBNB" """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """ Display "C" followed by text variable """
+        no_underscore = text.replace('_', ' ')
+        return f'C {no_underscore}'
+
+    @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text):
+        """ Display "Python" followed by text variable """
+        no_underscore = text.replace('_', ' ')
+        return f'Python {no_underscore}'
+
+    app.run(host='0.0.0.0', port=5000)
+
+
+if __name__ == "__main__":
+    start_flask()
