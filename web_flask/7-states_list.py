a2aa89db1ac5c4e16716ddf8934246600b4ca49f
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-18 15:32:13 -0700

    Flask app that loads an html page that list all states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,19 +3,23 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """tears down session"""
+def teardown(self):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
-def list_states():
-    states = storage.all("State")
-    return render_template('7-states_list.html', states=states.values())
+def states():
+    ''' Display the list of states '''
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
